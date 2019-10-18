b950057603940cfe09e27d33488f56862a73e5f8
Leidiane YVANOFF <lydesign@MacBook-Pro-de-Leidiane.local>
2019-10-18 21:29:58 +0200

    Exercices primitives

--- src/exercices/PrimitiveTraining.java
+++ src/exercices/PrimitiveTraining.java
@@ -1,18 +1,41 @@
-public class PrimitiveTraining {
+public class PrimitiveTraining { 
 
-    // TODO 1 : Change the method 'integer' to return the expected result : int 42
-    public static int integer() {
-        return 0;
-    }
+        public static int integer() {
+	        int integerTest = 42;
+	        return integerTest;
+	    }
+        public static String str() {
+        	String helloTest = "Hello";
+        	return helloTest;
+        }
+        public static double real() {
+        	double realTest = 3.14;
+        	return realTest;
+        }
+       
+        public static boolean bool() {
+        	boolean boolTest = false;
+        	return boolTest;
+        }
+        public static String nothing() {
+        	String nothingTest = null;
+        	return nothingTest;
+        }
+        
+        
+        
+	public static void main(String[] args ) {
+		
+		
+	    System.out.println(integer());	    
+            System.out.println(str());
+	    System.out.println(real());
+	    System.out.println(bool());
+	    System.out.println(nothing());
+	   
+	  }   
+  
+} 	
 
-    // TODO 2 : Create a method 'str', result expected : String "Hello"
 
 
-    // TODO 3 : Create a method 'real', result expected : double 3.14
-
-
-    // TODO 4 : Create a method 'bool', result expected : boolean false
-
-
-    // TODO 5 : Create a method 'nothing', no result expected
-}
